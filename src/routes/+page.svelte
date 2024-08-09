d7041e2cea30c8f5d659e55648dce5a95a28583b
Kayden <143221653+Kayden1412@users.noreply.github.com>
2024-08-09 01:08:13 +0000

    fix layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,19 @@
 <script>
-	import { Button, Card  } from 'svelte-ux';
+	import { Button, Card } from 'svelte-ux';
 	export let data;
 </script>
 
-<main class=" pl-2 pr-2 grid grid-cols-2 gap-4">
-	
-     {#each data.data as d}
-		   
-      <a href="/anime/{d.mal_id}">
-	    <Card class="flex">
-			<img 
-			  src={d.images.webp.image_url}
-			  >
-				<span class="mb-8">{d.title}</span>
-		</Card>
+<main class="pt-4 pl-2 pr-2 grid grid-cols-2 gap-4">
+	{#each data.data as d}
+		<a href="/anime/{d.mal_id}">
+			<Card class="relative">
+				<img src={d.images.webp.image_url} class="w-full h-auto" />
+				<span
+					class="truncate absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white px-2 py-1 text-center w-full"
+				>
+					{d.title}
+				</span>
+			</Card>
 		</a>
-	 {/each}
-	
+	{/each}
 </main>
