83abf6c6c44358436bcfc6a650b5deb27659d157
Kayden <143221653+Kayden1412@users.noreply.github.com>
2024-08-09 01:36:03 +0000

    f

--- src/routes/anime/[id]/+page.svelte
+++ src/routes/anime/[id]/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import { Card } from 'svelte-ux';
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
-<Card class="flex justify-center">
-   <img 
-     src={data.data.images.webp.image_url}
-     class="w-125px h-auto"
-     >
-</Card>
+<div class="mt-20 relative flex justify-center items-center">
+	<img src={data.data.images.webp.image_url} class="w-40 h-auto" />
+	<span
+		class="absolute -bottom-8 left-1/2 transform -translate-x-1/2 text-white text-lg px-2 py-1 text-center max-w-full truncate"
+	>
+		{data.data.title}
+	</span>
+  
+</div>
