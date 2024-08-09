2f126f5ac7a7405b1b26e57636115b1149e8a627
Kayden <143221653+Kayden1412@users.noreply.github.com>
2024-08-09 01:24:37 +0000

    change anime page

--- src/routes/anime/[id]/+page.server.ts
+++ src/routes/anime/[id]/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    
-    return {};
+export const load = (async ({params}) => {
+    const req = await fetch(`https://api.jikan.moe/v4/anime/${params.id}/full`)
+    const res = await req.json()
+    return res;
 }) satisfies PageServerLoad;
--- src/routes/anime/[id]/+page.svelte
+++ src/routes/anime/[id]/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import { Card } from 'svelte-ux';
     import type { PageData } from './$types';
     
     export let data: PageData;
 </script>
+
+<Card class="flex justify-center">
+   <img 
+     src={data.data.images.webp.image_url}
+     class="w-125px h-auto"
+     >
+</Card>
