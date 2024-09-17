eb95a07fc9f61e0834c6eceba33905e26e18d9fc
Kayden <143221653+Kayden1412@users.noreply.github.com>
2024-09-17 16:58:03 +0000

    feat: add cache

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,13 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    const req  = await fetch('https://api.jikan.moe/v4/top/anime')
-    const res = await req.json()
+export const load = (async ({ setHeaders }) => {
+    const req = await fetch('https://api.jikan.moe/v4/top/anime');
+    const res = await req.json();
+
+    // Set cache-control headers to cache for 24 hours
+    setHeaders({
+        'cache-control': 'public, max-age=86400' // 86400 seconds = 24 hours
+    });
+
     return res;
 }) satisfies PageServerLoad;
